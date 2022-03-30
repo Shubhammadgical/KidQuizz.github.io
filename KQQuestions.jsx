3d378cf9d3ff6d38f37c11594baa01d442756b0a
Shubhammadgical <Shubham.chaurasia@madgicaltechdom.com>
2022-03-30 13:25:42 +0530

    newdata

--- KQQuestions.jsx
+++ KQQuestions.jsx
@@ -1,21 +1,7 @@
 import React, {Component} from "react";
-import elephant from "./animals/elephant.svg";
-import croc from "./animals/croc.svg"
-import giraffe from "./animals/giraffe.svg";
-import gorilla from "./animals/gorilla.svg";
-import koala from "./animals/koala.svg";
-import tiger from "./animals/tiger.svg";
-import whale from "./animals/whale.svg";
-import polarbear from "./animals/polar-bear.svg";
-import cow from "./animals/cow.jpg";
-import goat from "./animals/goat.jpg";
-import lion from "./animals/lion.jpg";
-import parrot from "./animals/parrot.jpg";
-import rabbit from "./animals/rabbit.jpg";
-import bear from "./animals/bear.jpg";
-import duck from "./animals/duck.jfif";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from "react-bootstrap";
+import http from "./httpService";
 
 class KQQuestions extends Component{
     state={
@@ -23,27 +9,23 @@ class KQQuestions extends Component{
         name:"",
         err:"",
         text: "What is the name of the animal in above picture ?",
-        questions: [
-                {options: ["Elephant", "Dog", "Whale"],answer: 1,image:elephant},
-                {options: ["Cow", "Goat", "Crocodile"],answer: 3,image:croc},
-                {options: ["Lion", "Giraffe", "Cow"],answer: 2,image:giraffe},
-                {options: ["Gorilla", "Parrot", "Whale"],answer: 1,image:gorilla},
-                {options: ["Horse", "Koala", "Polarbear"],answer: 2,image:koala},
-                {options: ["Duck", "Koala", "Tiger"],answer: 3,image:tiger},
-                {options: ["Polarbear", "Whale", "Crocodile"],answer: 2,image:whale},
-                {options: ["Polarbear", "Koala", "Dog"],answer: 1,image:polarbear},
-                {options: [ "Goat", "Crocodile","Cow"],answer: 3,image:cow},
-                {options: ["Rabbit", "Goat", "Cow"],answer: 2,image:goat},
-                {options: ["Lion", "Dog", "Whale"],answer: 1,image:lion},
-                {options: ["Duck", "Parrot", "Polarbear"],answer: 2,image:parrot},
-                {options: ["Parrot", "Koala", "Rabbit"],answer: 3,image:rabbit},
-                {options: ["Polarbear", "Bear", "Parrot"],answer: 2,image:bear},
-                {options: ["Duck", "Koala", "Crocodile"],answer: 1,image:duck},
-              ],
-              qindex:0,
-              errcount:0,
-              data:[]
-           
+        questions: [],
+        qindex:0,
+        errcount:0,
+        data:[],
+        date:"",
+        time:"",
+        datetime:"",
+        error:2           
+    }
+    async fetchdata(){
+        let response=await http.get(`/allQuestions`);
+        let {data}=response;
+        this.setState({questions:data});
+        console.log(this.state.questions);
+    }
+    async componentDidMount(){
+        this.fetchdata();
     }
     handleChange=(e)=>{
         const {currentTarget: input}=e;
@@ -64,20 +46,51 @@ class KQQuestions extends Component{
                let fnd=s1.data.find(a=>a===s1.questions[index]);
                if(!fnd)s1.data.push(s1.questions[index]);
              }
-             console.log(s1.data)
+             s1.datetime=Date();
+            console.log(s1.datetime)
+            let index1=-1;
+            for(let i=14;i<s1.datetime.length;i++){
+                if(s1.datetime[i]===" "&&i<25){
+                    index1=i;
+                    s1.time=s1.datetime.substring(16,index1);
+                }
+            }
+            console.log(s1.time)
+            let index2=-1;
+            for(let i=12;i<s1.datetime.length;i++){
+                if(s1.datetime[i]===" "&&i<18){
+                    index2=i;
+                    s1.date=s1.datetime.substring(0,index2);
+                }
+            }
+            console.log(s1.date)
             s1.show=1;
         }
         this.setState(s1);
     }
-    handlebtn=(index)=>{
+     setdata=async()=>{
+         try{
+        let newdata={name:this.state.name,errcount:this.state.errcount,date:this.state.date,time:this.state.time}
+        let response=await http.post(`/newData`,newdata);
+        console.log(response.status);
+         }catch(err){
+            this.setState({show:3});
+            console.log(this.state.show)
+             this.setState({error:1});
+            console.log(this.state.error)
+         }
+    }
+    handlebtn=(opt)=>{
+        console.log(opt);
         let s1={...this.state};
-        if(index+1===s1.data[s1.qindex].answer){
+        if(opt===s1.data[s1.qindex].answer){
             console.log(s1.qindex);
             if(s1.qindex<4){
             s1.err=""
             s1.qindex++;
             }else{
                 s1.show=2;
+                this.setdata();
             }
         }else{
             s1.err="Your Answer is incorrect";
@@ -85,6 +98,7 @@ class KQQuestions extends Component{
         }
         this.setState(s1);
     }
+    
     handlerestart=()=>{
         let s1={...this.state};
         s1.name="";
@@ -96,11 +110,14 @@ class KQQuestions extends Component{
         this.setState(s1);
     }
     render(){
-        let {questions,show,name,err,qindex,errcount,text,data}=this.state;
+        let {questions,show,name,err,qindex,errcount,text,data,error}=this.state;
+        console.log(error);
+        console.log(show)
         if(show==0){
             return(
                 <div className="container text-center p-4">
                     <h4>Welcome in Kids Quizz</h4>
+                    
                     <div className="form-group">
                             <input type="text" className="form-control" id="name" name="name" placeholder="Enter Name" value={name} onChange={this.handleChange} />
                     </div>
@@ -109,30 +126,38 @@ class KQQuestions extends Component{
                     <button className="btn btn-primary" onClick={()=>this.handleenter(0,questions.length-1)}>Enter</button>
                 </div>
             )
-        }else if(show==1){
+        }else if(show==1&& questions.length>0){
             return(
             <div className="container text-center p-4">
+                {console.log(data[qindex].image)}
                 <Container style={{maxWidth:400,maxHeight:400,minHeight:200,minWidth:200}}>
-                    <img className="img-fluid" alt="amimal" src={data[qindex].image} >
-                    </img>
+                    <img className="img-fluid" alt="Please check your connection."  src={data[qindex].image} >
+                    </img><br/>
+                    
                 </Container><br/>
                 {text}<br/>
                 {err&&<span className="text-danger">{err}</span>}<br/><br/>
-                {data[qindex].options.map((opt,index)=>(
+                <button className="btn btn-primary mx-4" 
+                    onClick={()=>this.handlebtn(data[qindex].opt1)}>{data[qindex].opt1}<br/></button>
+                <button className="btn btn-primary mx-4" 
+                    onClick={()=>this.handlebtn(data[qindex].opt2)}>{data[qindex].opt2}<br/></button>
                     <button className="btn btn-primary mx-4" 
-                    onClick={()=>this.handlebtn(index)}>{opt}<br/></button>
-                    
-                ))}
+                    onClick={()=>this.handlebtn(data[qindex].opt3)}>{data[qindex].opt3}<br/></button>
                 <br/>
+               
             </div>
             )
-        }else{
+        }else if(show==2 && error!=1){
             return(
                 <div className="container text-center p-4">
                     <h4>Result Screen of {name}</h4>
                     <h5>Error Count : <span className="text-danger">{errcount}</span></h5>
                     <button className="btn btn-primary" onClick={()=>this.handlerestart()}>Restart</button>
                 </div>
+            )
+        }else{
+            return(
+                <h4 className="text-center p-4">Error in Fetching Data, Please check your connection.</h4>
             )
         }
     }
